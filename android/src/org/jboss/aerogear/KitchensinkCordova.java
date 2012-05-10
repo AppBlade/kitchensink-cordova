7f98b17b7f21abddbd46ea56d9120b2bb45749eb
Dylan James <Dylan.James@raizlabs.com>
2012-05-10 16:00:58 -0400

    Integrated AppBlade SDK using the jar from the AppBlade project.

--- android/src/org/jboss/aerogear/KitchensinkCordova.java
+++ android/src/org/jboss/aerogear/KitchensinkCordova.java
@@ -1,8 +1,10 @@
 package org.jboss.aerogear;
 
-import android.app.Activity;
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
-import org.apache.cordova.*;
+
+import com.appblade.framework.AppBlade;
 
 public class KitchensinkCordova extends DroidGap
 {
@@ -10,7 +12,22 @@ public class KitchensinkCordova extends DroidGap
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        String token = "c8223091173a8949c02ec6d5838f487b";
+        String secret = "cb29424385cb284b381d76261e538faf";
+        String uuid = "dd70ca7b-a841-48f2-bdee-7fcc0929e9f5";
+        String issuance = "1333051907";
+        
+        AppBlade.register(this, token, secret, uuid, issuance);
+        
         super.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+    @Override
+    public void onResume()
+    {
+    	super.onResume();
+    	AppBlade.authorize(this);
     }
 }
 
